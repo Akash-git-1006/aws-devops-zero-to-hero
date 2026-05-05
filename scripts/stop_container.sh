4e72a87f728749a6a0adbe2a1b60946d3238fb1c
Akash-git-1006 <akashv1006@gmail.com>
2026-05-05 17:56:55 +0100

    push1

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -6,7 +6,6 @@ set -e
 containerid = 'docker ps | awk -f " " '{print $1}''
 docker rm -f $containerid
 =======
-containerId = 'docker ps | awk -f " " '{print $1}''
-docker rm -f $containerId
+
 
 >>>>>>> 8baa5ddffe737b2d32f8267b6e6ddc75aff3a4fd
