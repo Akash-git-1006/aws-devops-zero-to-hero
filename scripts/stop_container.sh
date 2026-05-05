e09c16bac40f3746d943c7a98d12cd46946aa6c9
Akash-git-1006 <akashv1006@gmail.com>
2026-05-05 17:56:11 +0100

    push

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,11 @@
 set -e
 
 # Stop the running container (if any)
+<<<<<<< HEAD
+containerid = 'docker ps | awk -f " " '{print $1}''
+docker rm -f $containerid
+=======
 containerId = 'docker ps | awk -f " " '{print $1}''
 docker rm -f $containerId
+
+>>>>>>> 8baa5ddffe737b2d32f8267b6e6ddc75aff3a4fd
